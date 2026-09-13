08f29bfadb0a0e7e31a385d4e02bfe3b5cefca6a
Alejandro Rueda <alecz23@live.com>
2026-09-13 15:56:15 -0600

    feat: add push device model

---
+++ app/Models/PushDevice.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PushDevice extends Model
+{
+    protected $fillable = [
+        'token',
+        'platform',
+        'app_version',
+        'device_name',
+        'is_active',
+        'last_seen_at',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'last_seen_at' => 'datetime',
+    ];
+}
