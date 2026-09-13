0f3ab60aa38d19094a38d269df77c1d53bcca6a0
Alejandro Rueda <alecz23@live.com>
2026-09-13 17:32:25 -0600

    feat: support Firebase service account file

--- config/services.php
+++ config/services.php
@@ -24,6 +24,7 @@ return [
     ],
 
     'firebase' => [
+        'credentials_path' => env('FIREBASE_CREDENTIALS'),
         'project_id' => env('FIREBASE_PROJECT_ID'),
         'client_email' => env('FIREBASE_CLIENT_EMAIL'),
         'private_key' => env('FIREBASE_PRIVATE_KEY'),
