56aa2d16e6f74ac588bd48b953335f2991eb4b89
Alejandro Rueda <alecz23@live.com>
2026-09-14 23:38:45 -0600

    feat: store winner notification delivery data

--- app/Models/ContestEntry.php
+++ app/Models/ContestEntry.php
@@ -9,8 +9,8 @@ class ContestEntry extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['contest_id','device_key','listener_name','phone','selected_option','is_correct','is_winner','claim_code','claimed_at'];
-    protected $casts = ['is_correct'=>'boolean','is_winner'=>'boolean','claimed_at'=>'datetime'];
+    protected $fillable = ['contest_id','device_key','push_token','listener_name','phone','selected_option','is_correct','is_winner','claim_code','claimed_at','winner_notified_at'];
+    protected $casts = ['is_correct'=>'boolean','is_winner'=>'boolean','claimed_at'=>'datetime','winner_notified_at'=>'datetime'];
 
     public function contest() { return $this->belongsTo(Contest::class); }
 }
